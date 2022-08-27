23ac9a00b2a1c94a6582816b9a2075fbdf50edcc
Manish Bishowkarma <92390093+bkmanish01@users.noreply.github.com>
2022-08-27 07:31:16 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Menu 
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+// Form
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+// Cart
+let cartItem = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
